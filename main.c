a2e192e246792e1090c623065d825b0f0f2f7ec9
Stephano55 <kevinstephano14@gmail.com>
2024-04-17 21:30:46 +0200

    ajout des reponses

--- main.c
+++ main.c
@@ -19,11 +19,42 @@ double combinaison(int n, int k){
     comb = factoriel(n)/(factoriel(k)* factoriel(n-k));
     return comb;
 }
+void prQuestion(){
+    double nbrCasPossible;
+    nbrCasPossible = combinaison(7, 3) * combinaison(5, 2);
+    printf("le nombre de cas possible est %f", nbrCasPossible);
+}
+void deuxQuestion(){
+    double nbrCasPossible;
+    nbrCasPossible = combinaison(7, 3) * combinaison(1, 1)* combinaison(4, 1);
+    printf("le nombre de cas possible est %f", nbrCasPossible);
+}
+void trQuestion(){
+    double nbrCasPossible;
+    nbrCasPossible = combinaison(5, 3) * combinaison(5, 2);
+    printf("le nombre de cas possible est %f", nbrCasPossible);
+}
 int main()
 {
     int n, k;
+    int question;
     double casPossible;
-    printf("On doit former un comit» comprenant 3 math»maticiens et 2 phisiciens");
-    printf("sur la base dÌun groupe plus large, form» de\n 7 math»maticiens et 5 phisiciens");
+    printf("\n\n\tOn doit former un comit» comprenant 3 math»maticiens et 2 phisiciens");
+    printf("sur la base dÌun groupe plus large, form» de\n 7 math»maticiens et 5 phisiciens\n");
+    printf("\nQuel est le nombre des cas possible pour former ce comit» si : \n");
+    printf("\n1. Le comit» peut comprendre nÌimporte lequel des math»maticiens et des phisiciens \n");
+    printf("\n2. Un phisicien particulier doit-Õtre membre du comit».\n");
+    printf("\n3. Deux math»maticiens particuliers doivent Õtre exlus du comit».\n");
+    printf("a quelle question voulez vouz que le programme rÈponde? (1,2,3)");
+    scanf("%d",&question);
+    if (question == 1){
+        prQuestion();
+    }
+    else if (question == 2){
+        deuxQuestion();
+    }
+    else{
+        trQuestion();
+    }
     return 0;
 }
